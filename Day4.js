6797b280229f0abed683680600fb079027c703ff
neerajgudi95 <61921282+neerajgudi95@users.noreply.github.com>
2022-04-12 20:13:04 +0530

    Add files via upload

--- Day4.js
+++ Day4.js
@@ -1,3 +1,4 @@
+// Object property comparison
 var obj1 = { name: "Person 1", age: 5 };
 var obj2 = { age: 5, name: "Person 2" };
 
@@ -8,3 +9,19 @@ for (let key in obj1) {
   }
   console.log(obj2[key]);
 }
+
+// API request for country flags
+const httpReq = new XMLHttpRequest();
+
+httpReq.onreadystatechange = (res) => {
+  let data = [];
+  if (httpReq.status === 200) {
+    data = JSON.parse(httpReq.responseText);
+  }
+  data.forEach((country) => {
+    console.log(country.flag);
+  });
+};
+
+httpReq.open("GET", "https://restcountries.com/v2/all");
+httpReq.send();
